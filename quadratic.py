09c49b4d8c196b09dd7356be749af219785de10f
Grandmaster69 <wenqi2004@gmail.com>
2021-07-22 22:54:14 +0800

    Improved it so it works now

--- quadratic.py
+++ quadratic.py
@@ -1,17 +1,16 @@
-def quadratic():
-    a = int(input("Enter an integer: "))
-    b = int(input("Enter an integer: "))
-    c = int(input("Enter an integer: "))
+def quadratic(a,b,c):
     roots = []
     if (b**2-4*a*c)>=0:
-        r1 = (-b+(b**2-4*a*c)**1/2)/(2*a)
-        r2 = (-b-(b**2-4*a*c)**1/2)/(2*a)
-    if r1 == r2:
-        roots.append(r1)
-    else:
-        roots.append(r2)
+        r1 = (-b+(b**2-4*a*c)**(1/2))/(2*a)
+        r2 = (-b-(b**2-4*a*c)**(1/2))/(2*a)
+        if r1 == r2:
+            roots.append(r1)
+        else:
+            roots = [r1,r2]
     return (roots)
 
 if __name__ == '__main__':
     # put tests here and run this file
-    print(quadratic(1, 2, 3)) # idk what this returns
+    print(quadratic(1, 5, 6)) # idk what this returns
+    print(quadratic(1, 6, 8))
+    print(quadratic(2, -8, -24))
